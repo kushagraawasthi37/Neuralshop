27c5477b4774d759c003bf9a7c5a3b2890ad87fb
Kushagraawasthi37@gmail.com <Kushagraawasthi37@gmailcom>
2025-10-26 12:39:56 +0530

    Admin nav styling done

--- admin/src/component/Nav.jsx
+++ admin/src/component/Nav.jsx
@@ -26,18 +26,18 @@ function Nav() {
   };
 
   return (
-    <div className="w-full h-13 md:h-[70px] bg-[#dcdbdbf8] z-10 fixed top-0 flex  items-center justify-between px-3 md:px-[30px] overflow-x-hidden shadow-md shadow-black ">
+    <div className="w-screen h-13 z-100 md:h-[70px] bg-gradient-to-r from-[#10121a] via-[#1a1f2e] to-[#252940]  fixed top-0 flex items-center justify-between px-[30px] shadow-md shadow-black select-none transition-colors duration-500 ease-in-out">
       <div
         className="w-[30%]  flex items-center justify-start gap-1  md:gap-2.5 cursor-pointer "
         onClick={() => navigate("/")}
       >
         <img src={logo} alt="" className="w-8 md:w-[30px]" />
-        <h1 className="text-5 md:text-[20px] text-[black] font-sans font-bold ">
+        <h1 className="text-5 md:text-[20px] text-white font-sans font-bold ">
           NeuralShop
         </h1>
       </div>
       <button
-        className="text-[15px] hover:border-2 hover:scale-95 active:95 hover:bg-black border-[#89daea] cursor-pointer bg-[#000000ca] py-2 px-3 md:py-2.5 md:px-5 rounded-2xl text-white "
+        className="text-[12px] md:text-4 bg-yellow-500 hover:bg-yellow-600 active:bg-yellow-700 text-[#10121a] font-semibold py-2 px-4 md:py-2.5 md:px-6 rounded-lg md:rounded-2xl shadow-md hover:shadow-lg transition-all duration-300 cursor-pointer select-none"
         onClick={logOut}
       >
         LogOut
