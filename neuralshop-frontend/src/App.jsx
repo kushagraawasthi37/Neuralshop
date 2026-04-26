d6f563be5dae64827e17ce3f98f68c3f89d4e7d3
Kushagraawasthi37@gmail.com <Kushagraawasthi37@gmailcom>
2026-04-26 22:32:59 +0530

    frontend basic setup

---
+++ neuralshop-frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-red-500 text-white text-5xl h-screen flex items-center justify-center">
+      Tailwind Test 🚀
+    </div>
+  )
+}
+
+export default App
